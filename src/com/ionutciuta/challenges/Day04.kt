98d59a61315ea55b6e03d8150c81056f89bf671c
Ionuț Ciuta <ionutciuta24@gmail.com>
2020-12-04 23:34:27 +0200

    Day 4 part 2

--- src/com/ionutciuta/challenges/Day04.kt
+++ src/com/ionutciuta/challenges/Day04.kt
@@ -12,9 +12,9 @@ class Day04(file: String) {
         val passport = mutableMapOf<String, String>()
 
         for (line in data) {
-            if(line.isEmpty()) {
+            if(line.isBlank()) {
                 passports.add(passport)
-                if(isValidPassport(passport)) {
+                if(isValidPassport(passport, true)) {
                     count++
                 }
                 passport.clear()
@@ -30,9 +30,13 @@ class Day04(file: String) {
         return count
     }
 
-    private fun isValidPassport(passport: Map<String, String>): Boolean {
+    private fun isValidPassport(passport: Map<String, String>, extraValidation: Boolean = false): Boolean {
         PassportBlueprint.format.forEach {
-            if(it.value && !passport.containsKey(it.key)) {
+            if(it.value.isMandatory && !passport.containsKey(it.key)) {
+                return false
+            }
+
+            if(extraValidation && it.value.isMandatory && !it.value.validation(passport[it.key]!!)) {
                 return false
             }
         }
@@ -52,16 +56,54 @@ class Day04(file: String) {
 object PassportBlueprint {
     // field name -> mandatory?
     val format = mapOf(
-        ("byr" to true),
-        ("iyr" to true),
-        ("eyr" to true),
-        ("hgt" to true),
-        ("hcl" to true),
-        ("ecl" to true),
-        ("pid" to true),
-        ("cid" to false)
+        ("byr" to PassportFieldRule(validation = {
+            it.toInt() in 1920..2002
+        })),
+        ("iyr" to PassportFieldRule(validation = {
+            it.toInt() in 2010..2020
+        })),
+        ("eyr" to PassportFieldRule(validation = {
+            it.toInt() in 2020..2030
+        })),
+        ("hgt" to PassportFieldRule(validation = {
+            when {
+                it.contains("cm") -> isValidHeight("cm", 150, 193, it)
+                it.contains("in") -> isValidHeight("in", 59, 76, it)
+                else -> false
+            }
+        })),
+        ("hcl" to PassportFieldRule(validation = {
+            Regex("^(#[a-f0-9]{6})$").matches(it)
+        })),
+        ("ecl" to PassportFieldRule(validation = {
+            setOf("amb","blu","brn","gry","grn","hzl","oth").contains(it)
+        })),
+        ("pid" to PassportFieldRule(validation = {
+            Regex("[0-9]{9}").matches(it)
+        })),
+        ("cid" to PassportFieldRule(isMandatory = false, validation = { true }))
     )
+
+    private fun isValidHeight(unit: String, min: Int, max: Int, input: String): Boolean {
+        val split = input.split(unit)
+        if (split.size != 2)
+            return false
+
+        var height = 0
+        try {
+            height = split[0].toInt()
+        } catch (e: Exception) {
+            return false
+        }
+
+        return height in min..max
+    }
 }
+
+class PassportFieldRule(
+    val isMandatory: Boolean = true,
+    val validation: (String) -> Boolean
+)
 
 fun main(args: Array<String>) {
     val file = Tools.getInput(args)
