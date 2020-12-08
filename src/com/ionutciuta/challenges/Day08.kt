dac64c2e21a3756d0356ea55c4caf0019f8e79d2
Ionuț Ciuta <ionutciuta24@gmail.com>
2020-12-09 00:24:33 +0200

    Day 8 part 2 try #1

--- src/com/ionutciuta/challenges/Day08.kt
+++ src/com/ionutciuta/challenges/Day08.kt
@@ -9,20 +9,18 @@ class Day08(file: String): Challenge {
 
 
     override fun solve() {
-        val r = Program(data).run()
+        val r = LoopingProgram(data).run()
         println(r)
     }
 
     override fun solvePart2() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val r = SelfHealingProgram(data).run()
+        println(r)
     }
 }
 
-class Program(val code: List<String>) {
-    private val history = mutableSetOf<Int>()
-    private var accumulator = 0
-
-    fun run(): Int {
+class LoopingProgram(code: List<String>): Program(code) {
+    override fun run(): Int {
         var i = 0
         while (history.size != code.size) {
             if(history.contains(i)) break
@@ -45,10 +43,61 @@ class Program(val code: List<String>) {
         }
         return accumulator
     }
+}
 
-    private fun getInstruction(line: String) = line.substring(0, 3)
+class SelfHealingProgram(code: List<String>): Program(code) {
+    override fun run(): Int {
+        var i = 0
+        var prev = 0
+        var foundError = false
+        while (history.size != code.size) {
+            if(history.contains(i)) {
+                i = prev
+                foundError = true
+            } else {
+                history.add(i)
+                prev = i
+            }
 
-    private fun getParam(line: String) = line.substring(4).toInt()
+            val line = code[i]
+            var instr = getInstruction(line)
+            val param = getParam(line)
+            if(foundError) {
+                instr = switchInstr(instr)
+            }
+
+            when (instr) {
+                "nop" -> i++
+                "acc" -> {
+                    accumulator += param
+                    i++
+                }
+                "jmp" -> i += param
+            }
+
+            if (foundError) {
+                return accumulator
+            }
+        }
+        return accumulator
+    }
+
+    private fun switchInstr(instr: String): String {
+        return when(instr) {
+            "nop" -> "jmp"
+            "jmp" -> "nop"
+            else -> instr
+        }
+    }
+}
+
+abstract class Program(val code: List<String>) {
+    protected val history = mutableSetOf<Int>()
+    protected var accumulator = 0
+    protected fun getInstruction(line: String) = line.substring(0, 3)
+    protected fun getParam(line: String) = line.substring(4).toInt()
+
+    abstract fun run(): Int
 }
 
 fun main(args: Array<String>) {
